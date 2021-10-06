04942400794e9e80651cf2b795ab7669d61edbc9
lilyboughton <lily.boughton@hotmail.co.uk>
2021-10-06 17:02:33 +0100

    tidies up JS and sorts out links in contact section

--- index.js
+++ index.js
@@ -1,25 +1,29 @@
 document.getElementById('hamburgerButton').addEventListener('click', () =>{
     if(document.getElementById('hamburgerMenu').getAttribute('class') === 'hamburgerMenu2' ){
-        document.getElementById('hamburgerMenu').setAttribute('class', 'hamburgerMenu1')
-        document.getElementById('hamburgerButton').setAttribute('class', 'hamburgerButtonOpen')
-        document.getElementById('mobileNav').setAttribute('class', 'mobileNavOpen')
+        openHamburger()
     } else  {
-        document.getElementById('hamburgerMenu').setAttribute('class','hamburgerMenu2')
-        document.getElementById('hamburgerButton').setAttribute('class','hamburgerButtonClosed')
-        document.getElementById('mobileNav').setAttribute('class','mobileNav')
+        closeHamburger()
     }
 })
 
 document.querySelectorAll('.hamburgerLink').forEach(link => {
     link.addEventListener('click', () => {
-        document.getElementById('hamburgerMenu').setAttribute('class','hamburgerMenu2')
-        document.getElementById('hamburgerButton').setAttribute('class','hamburgerButtonClosed')
-        document.getElementById('mobileNav').setAttribute('class','mobileNav')
+        closeHamburger()
     })
 })
 
 document.getElementById('hamburgerMenu').addEventListener('click', () => {
+    closeHamburger()
+})
+
+function openHamburger() {
+    document.getElementById('hamburgerMenu').setAttribute('class', 'hamburgerMenu1')
+    document.getElementById('hamburgerButton').setAttribute('class', 'hamburgerButtonOpen')
+    document.getElementById('mobileNav').setAttribute('class', 'mobileNavOpen')
+}
+
+function closeHamburger () {
     document.getElementById('hamburgerMenu').setAttribute('class','hamburgerMenu2')
     document.getElementById('hamburgerButton').setAttribute('class','hamburgerButtonClosed')
     document.getElementById('mobileNav').setAttribute('class','mobileNav')
-})
+}
