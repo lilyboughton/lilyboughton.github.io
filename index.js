aa739696c2b46bb2fb21c2c6237c1d4d6b615a04
lilyboughton <lily.boughton@hotmail.co.uk>
2021-11-18 17:19:24 +0000

    adds project one

--- index.js
+++ index.js
@@ -4,12 +4,17 @@ document.querySelector('.hamburgerButtonClosed').addEventListener('click', () =>
 
 document.querySelector('.hamburgerButtonOpen').addEventListener('click', () =>{
     closeHamburger()
+    closeProjectsMenu()
 })
 
 document.querySelectorAll('.hamburgerLink').forEach(link => {
     link.addEventListener('click', () => {
         closeHamburger()
     })
+})
+
+document.querySelector('.portfolioHamburgerLink').addEventListener('click', () =>{
+    openProjectsMenu()
 })
 
 function openHamburger() {
@@ -22,4 +27,18 @@ function closeHamburger() {
     document.querySelector('.hamburgerButtonOpen').style.display = 'none';
     document.querySelector('.hamburgerButtonClosed').style.display = 'block';
     document.getElementById('hamburgerMenu').style.display='none';
+}
+
+function openProjectsMenu() {
+    document.querySelector('.portfolioHamburgerLink').style.display = 'none';
+    document.querySelectorAll('.projectHamburgerLink').forEach(link => {
+        link.style.display = 'block';
+    })
+}
+
+function closeProjectsMenu() {
+    document.querySelector('.portfolioHamburgerLink').style.display = 'block';
+    document.querySelectorAll('.projectHamburgerLink').forEach(link => {
+        link.style.display = 'none';
+    })
 }
